cca216812a90d6d6faa32cf7ceefeaf134284d41
ciminoce <ciminoce@yahoo.com.ar>
2022-06-14 09:03:55 -0300

    Agregar archivos de proyecto.

---
+++ OperacionesBasicasPOO.Consola/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OperacionesBasicasPOO.Entidades;
+
+namespace OperacionesBasicasPOO.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"{Calculadora.Calcular(4,2,'+')}");
+                Console.WriteLine($"{Calculadora.Calcular(4,2,'-')}");
+                Console.WriteLine($"{Calculadora.Calcular(4,2,'*')}");
+                Console.WriteLine($"{Calculadora.Calcular(4,0,'/')}");
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OperacionesBasicasPOO.Entidades/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace OperacionesBasicasPOO.Entidades
+{
+    public static class  Calculadora
+    {
+        public static double Calcular(int nro1, int nro2, char operacion)
+        {
+            switch (operacion)
+            {
+                case '+':
+                    return nro1 + nro2;
+                    break;
+                case '-':
+                    return nro1 - nro2;
+                    break;
+                case '*':
+                    return nro1 * nro2;
+                case '/':
+                    if (Validar(nro2))
+                    {
+                        return nro1 / nro2;
+                    }
+
+                    throw new DivideByZeroException("No se puede dividir por cero");
+                default:
+                    throw new ArgumentException("Operación no contemplada o caracter erróneo");
+            }
+        }
+
+        private static bool Validar(int nro2)
+        {
+            return nro2 != 0;
+        }
+    }
+}
